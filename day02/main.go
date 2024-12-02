a7a9f39cf0181623ad9fa0c84412868c0397dff7
John Schmitz <johngc2010@hotmail.com>
2024-12-02 08:33:27 -0300

    refactor

--- day02/main.go
+++ day02/main.go
@@ -14,7 +14,7 @@ func partOne(filePath string) (int, error) {
 	lists := make([][]int, n)
 
 	for i := 0; i < n; i++ {
-		numbers := strings.Split(lines[i], " ")
+		numbers := strings.Fields(lines[i])
 		lists[i] = make([]int, len(numbers))
 		for j := 0; j < len(numbers); j++ {
 			fmt.Sscanf(numbers[j], "%d", &lists[i][j])
@@ -29,44 +29,6 @@ func partOne(filePath string) (int, error) {
 	}
 
 	return total, nil
-}
-
-func partTwo(filePath string) (int, error) {
-	bytes, _ := os.ReadFile(filePath)
-	lines := strings.Split(strings.TrimSpace(string(bytes)), "\n")
-	n := len(lines)
-
-	lists := make([][]int, n)
-
-	for i := 0; i < n; i++ {
-		numbers := strings.Split(lines[i], " ")
-		lists[i] = make([]int, len(numbers))
-		for j := 0; j < len(numbers); j++ {
-			fmt.Sscanf(numbers[j], "%d", &lists[i][j])
-		}
-	}
-
-	total := 0
-	for i := 0; i < n; i++ {
-		if isSafe(lists[i]) {
-			total += 1
-			continue
-		}
-		for j := 0; j < len(lists[i]); j++ {
-			if isSafe(removeIndex(lists[i], j)) {
-				total += 1
-				break
-			}
-		}
-	}
-
-	return total, nil
-}
-
-func removeIndex(s []int, index int) []int {
-	ret := make([]int, 0)
-	ret = append(ret, s[:index]...)
-	return append(ret, s[index+1:]...)
 }
 
 func isSafe(input []int) bool {
@@ -111,4 +73,42 @@ func isSafe(input []int) bool {
 	}
 
 	return true
+}
+
+func partTwo(filePath string) (int, error) {
+	bytes, _ := os.ReadFile(filePath)
+	lines := strings.Split(strings.TrimSpace(string(bytes)), "\n")
+	n := len(lines)
+
+	lists := make([][]int, n)
+
+	for i := 0; i < n; i++ {
+		numbers := strings.Split(lines[i], " ")
+		lists[i] = make([]int, len(numbers))
+		for j := 0; j < len(numbers); j++ {
+			fmt.Sscanf(numbers[j], "%d", &lists[i][j])
+		}
+	}
+
+	total := 0
+	for i := 0; i < n; i++ {
+		if isSafe(lists[i]) {
+			total += 1
+			continue
+		}
+		for j := 0; j < len(lists[i]); j++ {
+			if isSafe(removeIndex(lists[i], j)) {
+				total += 1
+				break
+			}
+		}
+	}
+
+	return total, nil
+}
+
+func removeIndex(s []int, index int) []int {
+	ret := make([]int, 0)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
 }
