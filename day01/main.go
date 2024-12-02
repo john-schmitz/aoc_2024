5b976e56f16965e1847311aa67be5b7da1a4331b
John Schmitz <johngc2010@hotmail.com>
2024-12-01 22:36:17 -0300

    refactor

--- day01/main.go
+++ day01/main.go
@@ -1,107 +1,63 @@
 package main
 
 import (
-	"bufio"
+	"fmt"
 	"os"
-	"sort"
-	"strconv"
+	"slices"
 	"strings"
 )
 
 func partOne(filePath string) int {
-	lines, err := getLines(filePath)
-	if err != nil {
-		panic(err)
-	}
-	left, right, err := parseLines(lines)
-	if err != nil {
-		panic(err)
-	}
+	bytes, _ := os.ReadFile(filePath)
+	lines := strings.Split(strings.TrimSpace(string(bytes)), "\n")
+	n := len(lines)
 
-	sort.Slice(left, func(i, j int) bool {
-		return left[i] < left[j]
-	})
+	left := make([]int, n)
+	right := make([]int, n)
 
-	sort.Slice(right, func(i, j int) bool {
-		return right[i] < right[j]
-	})
-
-	total := 0
-
-	for i := 0; i < len(left); i++ {
-		x := Abs(left[i] - right[i])
-		total += x
+	for i := 0; i < n; i++ {
+		fmt.Sscanf(lines[i], "%d %d", &left[i], &right[i])
 	}
 
-	return total
+	slices.Sort(left)
+	slices.Sort(right)
+
+	sum := 0
+	for index := 0; index < n; index++ {
+		diff := left[index] - right[index]
+		if diff < 0 {
+			diff = -diff
+		}
+		sum += diff
+	}
+
+	return sum
 }
 
 func partTwo(filePath string) int {
-	lines, err := getLines(filePath)
-	if err != nil {
-		panic(err)
-	}
-	left, right, err := parseLines(lines)
-	if err != nil {
-		panic(err)
+	bytes, _ := os.ReadFile(filePath)
+	lines := strings.Split(strings.TrimSpace(string(bytes)), "\n")
+	n := len(lines)
+
+	left := make([]int, n)
+	right := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Sscanf(lines[i], "%d %d", &left[i], &right[i])
 	}
 
-	total := 0
+	sum := 0
 
-	for i := 0; i < len(left); i++ {
+	for index := 0; index < len(left); index++ {
 		occ := 0
-		for j := 0; j < len(right); j++ {
-			if left[i] == right[j] {
+		for jindex := 0; jindex < len(right); jindex++ {
+			if left[index] == right[jindex] {
 				occ += 1
 			}
 		}
 
-		total += left[i] * occ
+		sum += left[index] * occ
 	}
 
-	return total
-}
-
-func parseLines(lines []string) ([]int, []int, error) {
-	left := make([]int, 0)
-	right := make([]int, 0)
-	for _, s := range lines {
-		result := strings.Split(s, "   ")
-		value, err := strconv.Atoi(result[0])
-		if err != nil {
-			return nil, nil, err
-		}
-		left = append(left, value)
-
-		value, err = strconv.Atoi(result[1])
-		if err != nil {
-			return nil, nil, err
-		}
-
-		right = append(right, value)
-	}
-
-	return left, right, nil
-}
-
-func Abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-func getLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
+	return sum
 }
